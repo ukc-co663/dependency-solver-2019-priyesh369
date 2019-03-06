99a9fe887cc58c8192d2487c91f7c7c04ef2ae81
Priyesh Patel <prp6@kent.ac.uk>
2019-03-06 14:17:00 +0000

    Update Main.java

--- src/depsolver/Main.java
+++ src/depsolver/Main.java
@@ -11,24 +11,24 @@ import java.util.List;
 
 
 class Package {
-  private String name;
-  private String version;
-  private Integer size;
-  private List<List<String>> depends = new ArrayList<>();
-  private List<String> conflicts = new ArrayList<>();
+    private String name;
+    private String version;
+    private Integer size;
+    private List<List<String>> depends = new ArrayList<>();
+    private List<String> conflicts = new ArrayList<>();
 
-  //gets
-  public String getName() { return name; }
-  public String getVersion() { return version; }
-  public Integer getSize() { return size; }
-  public List<List<String>> getDepends() { return depends; }
-  public List<String> getConflicts() { return conflicts; }
-  //sets
-  public void setName(String name) { this.name = name; }
-  public void setVersion(String version) { this.version = version; }
-  public void setSize(Integer size) { this.size = size; }
-  public void setDepends(List<List<String>> depends) { this.depends = depends; }
-  public void setConflicts(List<String> conflicts) { this.conflicts = conflicts; }
+    //gets
+    public String getName() { return name; }
+    public String getVersion() { return version; }
+    public Integer getSize() { return size; }
+    public List<List<String>> getDepends() { return depends; }
+    public List<String> getConflicts() { return conflicts; }
+    //sets
+    public void setName(String name) { this.name = name; }
+    public void setVersion(String version) { this.version = version; }
+    public void setSize(Integer size) { this.size = size; }
+    public void setDepends(List<List<String>> depends) { this.depends = depends; }
+    public void setConflicts(List<String> conflicts) { this.conflicts = conflicts; }
 }
 
 // Psudeocode to implemenet
@@ -43,35 +43,35 @@ class Package {
 //     search(y)
 
 public class Main {
-  private static HashSet<List<string>> seen = new HashSet<>();
-  public static void main(String[] args) throws IOException {
-    TypeReference<List<Package>> repoType = new TypeReference<List<Package>>() {};
-    List<Package> repo = JSON.parseObject(readFile(args[0]), repoType);
-    TypeReference<List<String>> strListType = new TypeReference<List<String>>() {};
-    List<String> initial = JSON.parseObject(readFile(args[1]), strListType);
-    List<String> constraints = JSON.parseObject(readFile(args[2]), strListType);
+    private static HashSet<List<String>> seen = new HashSet<>();
+    public static void main(String[] args) throws IOException {
+        TypeReference<List<Package>> repoType = new TypeReference<List<Package>>() {};
+        List<Package> repo = JSON.parseObject(readFile(args[0]), repoType);
+        TypeReference<List<String>> strListType = new TypeReference<List<String>>() {};
+        List<String> initial = JSON.parseObject(readFile(args[1]), strListType);
+        List<String> constraints = JSON.parseObject(readFile(args[2]), strListType);
 
-    // CHANGE CODE BELOW:
-    // using repo, initial and constraints, compute a solution and print the answer
-    
-    for (Package p : repo) {
-      System.out.printf("package %s version %s\n", p.getName(), p.getVersion());
-      for (List<String> clause : p.getDepends()) {
-        System.out.printf("  dep:");
-        for (String q : clause) {
-          System.out.printf(" %s", q);
+        // CHANGE CODE BELOW:
+        // using repo, initial and constraints, compute a solution and print the answer
+
+        for (Package p : repo) {
+            System.out.printf("package %s version %s\n", p.getName(), p.getVersion());
+            for (List<String> clause : p.getDepends()) {
+                System.out.printf("  dep:");
+                for (String q : clause) {
+                    System.out.printf(" %s", q);
+                }
+                System.out.printf("\n");
+            }
         }
-        System.out.printf("\n");
-      }
+
+
     }
 
-    
-  }
-
-  static String readFile(String filename) throws IOException {
-    BufferedReader br = new BufferedReader(new FileReader(filename));
-    StringBuilder sb = new StringBuilder();
-    br.lines().forEach(line -> sb.append(line));
-    return sb.toString();
-  }
+    static String readFile(String filename) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(filename));
+        StringBuilder sb = new StringBuilder();
+        br.lines().forEach(line -> sb.append(line));
+        return sb.toString();
+    }
 }
